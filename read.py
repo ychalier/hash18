fe83e4ac87bf800b5e2e3800b097abf5e1725503
ychalier <yohan.chalier@telecom-paristech.fr>
2018-03-01 19:11:49 +0100

    add display

--- read.py
+++ read.py
@@ -9,6 +9,14 @@ class Input:
         self.T = int(split[5])
         self.rides = []
 
+    def __str__(self):
+        s = str(self.R) + " rows\n" + str(self.C) + " columns\n" + str(self.F)\
+            + " vehicules\n" + str(self.N) + " rides\n" + str(self.B)\
+            + " bonus\n" + str(self.T) + " steps\n"
+        for ride in self.rides:
+            s += str(ride) + "\n"
+        return s
+
 
 class Ride:
     def __init__(self, line):
@@ -17,6 +25,11 @@ class Ride:
         self.finish = int(split[2]), int(split[3])
         self.earlier_start = int(split[4])
         self.latest_finish = int(split[5])
+
+    def __str__(self):
+        return "ride from " + str(self.start) + " to " + str(self.finish)\
+               + ", earliest start " + str(self.earlier_start)\
+               + ", latest finish " + str(self.latest_finish)
 
 
 def read_file(filename):
@@ -29,4 +42,4 @@ def read_file(filename):
     return inp
 
 
-print(read_file("a_example.in"))
+print(read_file("b_should_be_easy.in"))
